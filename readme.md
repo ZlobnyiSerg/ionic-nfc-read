061db2af4833b621f8551041ee42706a9e4fe9b9
ZlobnyiSerg <zlobnyiserg@gmail.com>
2020-06-19 12:41:27 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## How this project was initialized
+`ionic start` and then Tabs-application with angular template was selected.
+
+## Additional steps to init NFC
+All regarding documentation (https://ionicframework.com/docs/native/nfc/):
+1. Run `ionic cordova plugin add phonegap-nfc`
+2. Run `npm install @ionic-native/nfc`
+3. Initialized on platform-ready event:
+```angular2
+
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,64 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {AlertController, Platform, ToastController} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {NFC} from '@ionic-native/nfc/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private alertCtrl: AlertController,
+        private nfc: NFC,
+        private toast: ToastController
+    ) {
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+            // this.checkNfc();
+        });
+    }
+
+    async checkNfc() {
+        /*if (await this.nfc.enabled()) {
+            const toast = await this.toast.create({
+                message: 'NFC Listener activated',
+                duration: 1000
+            });
+            await toast.present();
+        } else {
+            const alert = await this.alertCtrl.create({
+                subHeader: 'NFC disabled',
+                buttons: [{text: 'OK'}, {
+                    text: 'GOTO SETTINGS',
+                    handler: async () => {
+                        await this.nfc.showSettings();
+                    }
+                }]
+            });
+            await alert.present();
+        }*/
+    }
+
+    async addListenNFC() {
+        /*this.nfc.addTagDiscoveredListener(nfcEvent => {
+            console.log('sesReadNFC', nfcEvent.tag);
+        }).subscribe(data => {
+            if (data && data.tag && data.tag.id) {
+                const tagId = this.nfc.bytesToHexString(data.tag.id);
+                console.log(tagId);
+            }
+        });*/
+    }
 }
